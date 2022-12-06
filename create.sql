11db67699a4ab33727dd4ef28885a2d1b90bcbef
Sofia Alfsson <sofia.alfsson.1262@student.uu.se>
2022-12-06 16:29:53 +0100

    changed some stuff

--- create.sql
+++ create.sql
@@ -16,10 +16,10 @@ INSERT INTO Department (Dep_ID, Title, Desc_of_dep) VALUES ('Books', 'Magazines'
 INSERT INTO Department (Dep_ID, Title, Desc_of_dep) VALUES ('Books', 'Horror', 'Scary');
 
 -- Products
-INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('5', 'bok', 'books', '4', Null, Null, 'Books');
-INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('6', 'magazine', 'news', '4', Null, Null, 'Magazines');
-INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('7', 'headphones', 'electronics', '4', Null, Null, 'Cables');
-INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('8', 'Laptop', 'Computer', '4', Null, Null, 'Computers');
+INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('5', 'Science Book', 'Book of science', '4', '120', Null, 'Books');
+INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('6', 'Magazine', 'News magazine', '4', '20', Null, 'Magazines');
+INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('7', 'Headphones', 'Electronics', '4', '300', Null, 'Cables');
+INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('8', 'Laptop', 'Computer', '4', '1500', Null, 'Computers');
 INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('9', 'Newspaper', 'News', '7', '10', Null, 'Magazines');
 INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('10', 'Phone', 'Talk', '8', '2000', Null, 'Phones');
 INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('11', 'TV', 'Watch', '7', '3000', Null, 'Tvs');
@@ -27,10 +27,17 @@ INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_ra
 INSERT INTO Product (Product_ID, Product_desc, Stock_quantity, Price, Average_rating, Depart_ID) VALUES ('13', 'Cable', 'Long', '7', '200', Null, 'Cables');
 
 --Review
-INSERT INTO Review (Review_ID, Star_rating, Longer_review, Product_ID) VALUES ('1', '4', 'fin','4');
-INSERT INTO Review (Review_ID, Star_rating, Longer_review, Product_ID) VALUES ('2', '2', 'ful','5');
+INSERT INTO Review (Review_ID, Star_rating, Longer_review, Product_ID) VALUES ('1', '4', 'Good computer','8');
+INSERT INTO Review (Review_ID, Star_rating, Longer_review, Product_ID) VALUES ('2', '2', 'Boring book','5');
 
 --User
-INSERT INTO User (SNN, Name, Email, Adress, Password, Phone_number, User_Agreement, Ordered_Product_ID, Review_ID) VALUES ('4', 'Cissi', 'c', 'Uppsala', 'hej', '23', '1', '4', '1');
-INSERT INTO User (SNN, Name, Email, Adress, Password, Phone_number, User_Agreement, Ordered_Product_ID, Review_ID) VALUES ('5', 'Sofia', 'c', 'Uppsala', 'hej', '23', '1', '5', '2');
+INSERT INTO User (SNN, Name, Email, Adress, Password, Phone_number, User_Agreement, Ordered_Product_ID, Review_ID) VALUES ('1234', 'Cissi', 'cissi@mail.com', 'Uppsala', 'hej', '123456', '1', '8', '1');
+INSERT INTO User (SNN, Name, Email, Adress, Password, Phone_number, User_Agreement, Ordered_Product_ID, Review_ID) VALUES ('5678', 'Sofia', 'sofia@mail.com', 'Stockholm', 'hej', '789101', '1', '5', '2');
 
+--Featured products
+INSERT INTO Featured_dep (Title, Short_desc, Dep_ID) VALUES ('Electronics', 'All kinds of electronics!','Electronics');
+INSERT INTO Featured_dep (Title, Short_desc, Dep_ID) VALUES ('Books', 'Lots of good books','Books');
+
+--Featured products
+INSERT INTO Featured_prod (Title, Description, CurrentPrice, Featured_Prod_ID) VALUES ('Laptop', 'Computer','1500','8');
+INSERT INTO Featured_prod (Title, Description, CurrentPrice, Featured_Prod_ID) VALUES ('Science Book', 'Book of science', '20','5');
--- databas.sql
+++ databas.sql
@@ -6,8 +6,6 @@ Adress VARCHAR(255),
 Password VARCHAR(255),
 Phone_number int,
 User_Agreement bool,
-Ordered_Product_ID int,
-Review_ID int,
 PRIMARY KEY (SSN)
 );
 
@@ -16,7 +14,8 @@ Review_ID int NOT NULL,
 Star_rating int,
 Longer_review VARCHAR(255),
 Product_ID int,
-PRIMARY KEY (Review_ID)
+PRIMARY KEY (Review_ID),
+FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
 );
 
 CREATE TABLE Product(
@@ -24,7 +23,7 @@ Product_ID int NOT NULL,
 Title VARCHAR(255),
 Product_desc VARCHAR(255),
 Stock_quantity int,
-Price int,
+Price int NOT NULL,
 Average_rating int,
 Depart_ID VARCHAR(255)
 PRIMARY KEY (Product_ID)
@@ -50,3 +49,14 @@ Ordered_product_id int,
 PRIMARY KEY (Order_ID),
 FOREIGN KEY (Ordered_product_id) REFERENCES Product(Product_ID)
 );
+
+CREATE TABLE Featured_prod(
+    Title VARCHAR(255),
+    Ft_Desc VARCHAR(255),
+    CurrentPrice int NOT NULL,
+    Featured_Prod_ID int,
+    PRIMARY KEY (?),
+    FOREIGN KEY (Featured_Prod_ID) REFERENCES Product(Product_ID)
+)
+
+CREATE TABLE Featured_prod
